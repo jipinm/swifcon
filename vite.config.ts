5c428d5b822b4d7475e84640c173960224e22228
Jipin Kumar M <jipinm@gmail.com>
2025-06-11 09:42:46 +0530

    Add GitHub Pages deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/swifcon/', // 👈 for git deployment
   plugins: [react()],
 })
