3663ed900a200a775d7edc98b997905b0c9bda53
Jipin Kumar M <jipinm@gmail.com>
2025-06-11 10:10:32 +0530

    Add GitHub Pages base path added

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/swifcon/',
   plugins: [react()],
   // Fix asset paths for build
   publicDir: 'public',
